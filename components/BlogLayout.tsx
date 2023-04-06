12616321a412fc5a7e4551f4afadfaf4f122aeaa
Devon <devon.coates@totaleto.com>
2023-04-06 17:37:25 -0600

    changes

--- components/BlogLayout.tsx
+++ components/BlogLayout.tsx
@@ -1,5 +1,11 @@
+import FiberNewRoundedIcon from '@mui/icons-material/FiberNewRounded'
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded'
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded'
+import ShoppingBasketRoundedIcon from '@mui/icons-material/ShoppingBasketRounded'
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded'
 import AlertBanner from 'components/AlertBanner'
 
+import NavBar from './NavBar'
 export default function BlogLayout({
   preview,
   loading,
@@ -13,7 +19,17 @@ export default function BlogLayout({
     <>
       <div className="min-h-screen">
         <AlertBanner preview={preview} loading={loading} />
-        <main>{children}</main>
+        <NavBar
+        navItems={[
+          { name: 'Home', icon: <HomeRoundedIcon /> },
+          { name: 'About', icon: <InfoRoundedIcon /> },
+          { name: 'Purchasing', icon: <ShoppingBasketRoundedIcon /> },
+          { name: 'New Litters', icon: <FiberNewRoundedIcon /> },
+          { name: 'Shop', icon: <ShoppingCartRoundedIcon /> },
+        ]}
+      />
+      {children}
+      {/* <Footer /> */}
       </div>
     </>
   )
---
+++ components/NavBar.tsx
@@ -0,0 +1,104 @@
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Paper,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import { useRouter } from 'next/router'
+// import Link from 'next/link'
+import * as React from 'react'
+
+import Logo from '../public/logo.svg'
+
+interface INavItems {
+  name: string
+  icon: React.ReactNode
+}
+
+interface Props {
+  navItems: INavItems[]
+}
+
+// const LogoComponent = (props: any) => <Logo {...props} />
+
+function NavBar(props: Props) {
+  const { navItems } = props
+  const router = useRouter()
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const [value, setValue] = React.useState(0)
+
+  const _handleNavigate = (item: string) => {
+    if (item === 'Home') {
+      return router.push(`/`)
+    }
+    router.push(`/${item.replace(/\s+/, '')}`)
+  }
+
+  return (
+    <>
+      {/* ( */}
+      <AppBar component="nav" color="default">
+        <Toolbar>
+          <Box
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: { height: '60px' },
+              pt: 0.5,
+            }}
+          >
+            Test
+            {/* <LogoComponent onClick={() => _handleNavigate('Home')} /> */}
+          </Box>
+          <>
+            {!isMobile && (
+              <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                {navItems.map((item) => (
+                  <Button
+                    key={item.name}
+                    sx={{ color: 'text.primary' }}
+                    onClick={() => _handleNavigate(item.name)}
+                  >
+                    {item.name}
+                  </Button>
+                ))}
+              </Box>
+            )}
+          </>
+        </Toolbar>
+      </AppBar>
+      {isMobile && (
+        <Paper
+          sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+          elevation={3}
+        >
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue)
+            }}
+          >
+            {navItems.map((item) => (
+              <BottomNavigationAction
+                key={item.name}
+                label={item.name}
+                icon={item.icon}
+                onClick={() => _handleNavigate(item.name)}
+              />
+            ))}
+          </BottomNavigation>
+        </Paper>
+      )}
+    </>
+  )
+}
+
+export default NavBar
